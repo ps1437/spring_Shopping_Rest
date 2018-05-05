fba779d06b7e57dfa41beb11d5331fb7c29d76a0
ps1437 <praveen369soni@gmail.com>
2018-05-05 15:17:10 +0530

    initial

---
+++ src/main/java/com/syscho/shoping/main/ShopingWorldApplication.java
@@ -0,0 +1,12 @@
+package com.syscho.shoping.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingWorldApplication.class, args);
+	}
+}
